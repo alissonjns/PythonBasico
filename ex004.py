014a6ef4e3b96be629b073a94fa7aca6f1a9e657
Alisson-Jns <alisson.junio@hotmail.com>
2022-06-20 21:53:37 -0300

    Exercicio dissecando variaveis

---
+++ ex004.py
@@ -0,0 +1,11 @@
+a = input("digite algo: ")
+print("O tipo primitivo desse valor e ", type(a))
+print("So tem espaços {} ", a.isspace())
+print("E numerico ", a.isnumeric())
+print("E alfabetico ", a.isalnum())
+print("E alfanumerico ", a.isalnum())
+print("Esta em maisculas ", a.isupper())
+print("Esta em minusculas ", a.islower())
+print("Esta capitalizada ", a.istitle())
+
+
