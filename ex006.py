d7f9b131dbc00f6d7db832ba7640e03d2412de9e
Alisson-Jns <alisson.junio@hotmail.com>
2022-06-21 20:49:33 -0300

    Exercicio fazendo o dobro, triplo e raiz quadrada de um numero

---
+++ ex006.py
@@ -0,0 +1,12 @@
+n = int(input("Digite um numero: "))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print(" O numero digitado foi {} \n O seu dobro e igual a {} \n O seu triplo equivale a {} \n "
+"Sua raiz quadradra e igual a {:.2f} ".format(n, d, t, r))
+n = int(input("Digite um numero: "))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print("O numero digitado foi {} \n O seu dobro e igual a {} \n "
+"O seu triplo e igual a {} \n E sua raiz quadrada e {:.2f}".format(n, d, t, r))
