4d4bfcebc4d76c8ca3a14d26dd39d64832305638
Alisson-Jns <alisson.junio@hotmail.com>
2022-07-30 13:49:43 -0300

    Exercicio calculando a media aritmetica

---
+++ ex007.py
@@ -0,0 +1,4 @@
+n1 = float(input("Digite a primeiro nota do aluno: "))
+n2 = float(input("Digite a segunda nota do aluno: "))
+media = float (n1+n2)/2
+print("O aluno tirou {} e {} sua media foi de {}".format(n1, n2, media))
